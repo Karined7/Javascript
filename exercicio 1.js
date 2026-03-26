52044bb2a393e0a2348e864615b8a17f46087cc9
Karine7 <karine.dorigon7@gmail.com>
2026-03-25 21:21:05 -0300

    subindo primeiro exercicio 1

--- exercicio 1.js
+++ exercicio 1.js
@@ -1,4 +1,17 @@
 let largura = 10;
 let altura = 5;
-let area = 10 * 5;
-console.long ( )
+let area = largura * altura ;
+console.log("A área é:?" + area);
+
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 > num2); // false
+console.log(num1 < num2); // true
+console.log(num1 == num2); // false
+console.log(num1 != num2); // true
+
+
+let celsius = 25;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(celsius = "ºC = " + fahrenheit + "ºF");
