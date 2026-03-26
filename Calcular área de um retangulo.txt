77376188a5842931454776e6774d1ee88e888ede
Karine7 <karine.dorigon7@gmail.com>
2026-03-25 21:57:47 -0300

    subindo exercicio 1

---
+++ Calcular área de um retangulo.txt
@@ -0,0 +1,4 @@
+let largura = 10;
+let altura = 5;
+let area = 10 * 5;
+console.long ( )
--- exercicio 1.js
+++ exercicio 1.js
@@ -15,3 +15,16 @@ console.log(num1 != num2); // true
 let celsius = 25;
 let fahrenheit = (celsius * 9/5) + 32;
 console.log(celsius = "ºC = " + fahrenheit + "ºF");
+
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 8.5;
+let media = (nota1 + nota2 + nota3) / 3;
+console.log ("Média da nota:", media.toFixed(1));
+
+
+let valorEmReais = 100;
+let cotacaoDolar = 5.00;
+let valorEmDolares = valorEmReais / cotacaoDolar;
+console.log(valorEmReais + " reais equivalem a " + valoresEmDolares.toFixed(2) + "doláres");
+
